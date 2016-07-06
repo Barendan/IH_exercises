c74d89cf8c03da5a9c0860b6e2f99d64a37e77d7
barendan <Barendan@gmail.com>
2016-07-06 09:55:47 -0400

    Added regex checks for phone and email to the contact list

---
+++ Week4/TimeTrackher/address_book/app/controllers/contacts_controller.rb
@@ -0,0 +1,34 @@
+class ContactsController < ApplicationController
+
+	def new
+		render "new"
+	end
+
+	def index
+		@contacts_array = Contact.order(name: "asc") 		
+		render "index"
+	end
+
+	def create
+		contact = Contact.new(
+			:name => params[:contact][:name],
+			:address => params[:contact][:address],
+			:phone_number => params[:contact][:phone_number],
+			:email_address => params[:contact][:email])
+
+		email_pattern = /^\w+@\w+\.[A-Za-z]+$/
+		phone_pattern = /(\+1 )?\(?[0-9]{3}\)? ?-?[0-9]{3}? ?-?[0-9]{4}/
+
+		if (contact.email_address =~ email_pattern) != nil && (contact.phone_number = phone_pattern)
+			contact.save
+			redirect_to("/contacts")
+		else
+			redirect_to("/contacts/new")
+		end
+	end
+
+	def show
+		@contact_info = Contact.find(params[:id])
+		render 'show'	
+	end
+end
---
+++ Week4/TimeTrackher/address_book/db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+contact1 = Contact.create(name: "Morgan",address: "123 s cherry lane",phone_number: "305 414 9556",email_address: "dlangsam@hotmail.com")
+contact2 = Contact.create(name: "Johnny",address: "44 winged foot dr",phone_number: "954 232 1978",email_address: "ddanielito@hotmail.com")
+contact3 = Contact.create(name: "Carl",address: "314 east coast park",phone_number: "305 123 2246",email_address: "yaboycarl11@gmail.com")
+contact4 = Contact.create(name: "Nick",address: "1500 s ocean dr",phone_number: "786 419 1557",email_address: "nickiesgotagun@gmail.com")
